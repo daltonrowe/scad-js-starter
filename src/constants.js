03d6077cb4784673d45c46896b05f90cbcef04e2
Dalton <cdaltonrowe@gmail.com>
2025-03-06 10:29:45 +0100

    remove unused update render

--- src/constants.js
+++ src/constants.js
@@ -13,13 +13,5 @@ export const bodyHeight = 30;
 export const bodyThickness = 6.5;
 export const bodyLength = 140;
 
-export const padHeight = 16;
-export const padWidth = 24;
-export const padDepth = 1;
-
 export const holeRadius = 1.6;
 export const holeHeight = bodyThickness + nothing;
-
-export const pizzaWidth = 20;
-export const pizzaDepth = 20;
-export const pizzaHeight = 10;
